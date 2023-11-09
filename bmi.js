8a7c1915a2919b47c95321840242889aecc79573
Misaokilife <misaokilove2@gmail.com>
2023-11-09 18:26:34 +0900

    kadai_005を追加

---
+++ bmi.js
@@ -0,0 +1,14 @@
+/*[体重(kg)]÷[身長(m)×身長(m)]
+体重68kg　身長1.7m */
+
+let yourWeight;
+yourWeight = 68;
+
+let yourHeight;
+yourHeight = 1.7;
+
+let yourBmi;
+yourBmi = yourWeight / (yourHeight * yourHeight);
+
+console.log(yourBmi);
+

4071f24b2fdd4c60ad904e8f2897e5ddad824917
Misaokilife <misaokilove2@gmail.com>
2023-11-17 22:48:16 +0900

    kadai_020を追加を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const Btn = document.getElementById('btn');
+const showText = document.getElementById('text');
+
+Btn.addEventListener('click', () => {
+    showText.textContent = 'ボタンをクリックしました';
+});
+
